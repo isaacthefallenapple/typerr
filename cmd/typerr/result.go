b6fcfff82ce0232043736754a95e497e0cb46389
isaacthefallenapple <isaacthefallenapple@gmail.com>
2019-12-17 19:01:18 +0100

    Add WPM stat to result

--- cmd/typerr/result.go
+++ cmd/typerr/result.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
 
@@ -28,10 +29,21 @@ func (r *result) Ratio() float64 {
 func (r *result) MPS() float64 {
 	return float64(r.mistakes) / r.time.Seconds()
 }
+
+func (r *result) WPM() float64 {
+	var words int
+	for _, input := range r.input {
+		words += len(strings.Fields(input))
+	}
+	return float64(words) / r.time.Minutes()
+}
+
 func (r result) String() string {
 	plural := "s"
 	n := r.mistakes
-	if n == 1 { plural = "" }
-	return fmt.Sprintf("You made %d mistake%s in %s. That is a %.2f%% error rate at %.2f errors per second.",
-		n, plural, r.time.Truncate(time.Millisecond).String(), r.Ratio()*100, r.MPS())
+	if n == 1 {
+		plural = ""
+	}
+	return fmt.Sprintf("You made %d mistake%s in %s. That is a %.2f%% error rate at %.2f errors per second.\n\nYou typed at %.2f words per minute.",
+		n, plural, r.time.Truncate(time.Millisecond).String(), r.Ratio()*100, r.MPS(), r.WPM())
 }
